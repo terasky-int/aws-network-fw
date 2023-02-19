129ae8e9e93f6f9aa9065a369f548124a7bd204f
andreyv-terasky <andreyv@terasky.com>
2023-02-19 21:00:51 +0200

    create subnet mapping

--- main.tf
+++ main.tf
@@ -1,9 +1,23 @@
-# Create Firewall
+# Create subnet 
+data "aws_subnet" "subnet" {
+  vpc_id = var.vpc_id
+}
 
+
+
+# Create Firewall
 resource "aws_networkfirewall_firewall" "network_firewall" {
   name = var.firewall_name
   vpc_id = var.vpc_id
   firewall_policy_arn = aws_networkfirewall_firewall_policy.network_firewall_policy1
+    dynamic "subnet_mapping" {
+    for_each = data.aws_subnet.subnet[*].id
+
+    content {
+      subnet_id = subnet.value
+    }
+  }
+
 }
 
 # Create Firewall policy
@@ -13,6 +27,7 @@ resource "aws_networkfirewall_firewall_policy" "network_firewall_policy1" {
   firewall_policy {
     stateful_default_actions = [ "aws:pass" ]
     stateless_fragment_default_actions = [ "aws:pass" ]
+    stateless_default_actions = [ "aws:pass" ]
   }
 }
 
