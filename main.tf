13d1a52d11958905c8efedbaf8bcb88033f4925c
andreyv-terasky <andreyv@terasky.com>
2023-02-20 08:59:44 +0200

    create policy

--- main.tf
+++ main.tf
@@ -43,16 +43,30 @@
 # }
 
 
-resource "aws_networkfirewall_firewall_policy" "example_policy" {
-  name        = var.policy_name
-  description = "Example firewall policy"
+resource "aws_networkfirewall_firewall_policy" "policy_allow_all" {
+  name = var.policy_name
+
   firewall_policy {
-    stateless_default_actions = ["aws:pass"]
-    stateless_rule_groups = [
-      {
-        priority = 1
-        rule_group_arn = "aws:aws:policy/allow_all"
+    stateless_default_actions          = ["aws:pass"]
+    stateless_fragment_default_actions = ["aws:drop"]
+    stateless_rule_group_reference {
+      priority     = 1
+      resource_arn = aws_networkfirewall_rule_group.rule_group.arn
+    }
+  }
+}
+
+resource "aws_networkfirewall_rule_group" "rule_group" {
+  capacity = 100
+  name     = "example"
+  type     = "STATEFUL"
+  rule_group {
+    rules_source {
+      rules_source_list {
+        generated_rules_type = "DENYLIST"
+        target_types         = ["HTTP_HOST"]
+        targets              = ["ynet.co.il"]
       }
-    ]
+    }
   }
 }
