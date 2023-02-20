779d916c104dc7ed3b4f9778d86ca7636acb2d20
andreyv-terasky <andreyv@terasky.com>
2023-02-20 09:40:15 +0200

    sdf

--- main.tf
+++ main.tf
@@ -7,41 +7,8 @@ resource "aws_networkfirewall_firewall" "network_firewall" {
   firewall_policy_arn = aws_networkfirewall_firewall_policy.policy_allow_all.arn
   subnet_mapping {
     subnet_id = "subnet-0c2edb2d08b2af71c"
-
 }
 }
-# # Create Firewall policy
-
-# resource "aws_networkfirewall_firewall_policy" "network_firewall_policy1" {
-#   name = var.policy_name
-#   firewall_policy {
-#     stateful_default_actions = [ "aws:pass" ]
-#     stateless_fragment_default_actions = [ "aws:drop" ]
-#     stateless_default_actions = {
-      
-#     }
-#   }
-# }
-
-
-# resource "aws_networkfirewall_firewall_policy" "network_firewall_policy1" {
-#   name = var.policy_name
-
-#   firewall_policy {
-#     stateless_default_actions          = ["aws:pass"]
-#     stateless_fragment_default_actions = ["aws:drop"]
-#     stateless_rule_group_reference {
-#       priority     = 1
-#       resource_arn = aws_networkfirewall_rule_group.example.arn
-#     }
-#   }
-
-#   tags = {
-#     Tag1 = "Value1"
-#     Tag2 = "Value2"
-#   }
-# }
-
 
 resource "aws_networkfirewall_firewall_policy" "policy_allow_all" {
   name = var.policy_name
